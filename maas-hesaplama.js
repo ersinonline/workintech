5f72330fb574bbcdf4d87b9b3a3c177b9f0617fb
Ersin Çelik <clk.ersinnn@gmail.com>
2024-10-16 22:21:56 +0300

    Create maas-hesaplama.js

---
+++ maas-hesaplama.js
@@ -0,0 +1,6 @@
+function sumPersonelSalaries(maas1, maas2, maas3, maas4, maas5) {
+  return maas1 + maas2 + maas3 + maas4 + maas5;
+}
+
+let sonuc = sumPersonelSalaries(5000, 5000, 4000, 3000, 2000);
+console.log(sonuc); // Sonucu ekrana yazdır
